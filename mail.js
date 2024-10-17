2f2a86ca4a9cc74c6134a2ca1b2e1d9565a4e9ed
MattiaEsposito98 <esposito.matty@hotmail.it>
2024-10-17 13:41:39 +0200

    1 esercizio controllo email

---
+++ mail.js
@@ -0,0 +1,16 @@
+console.log("mail")
+
+let emailTot = []
+let newEmail = prompt("Inserisci la tua email")
+console.log("Email utente:" + newEmail)
+
+const isPresent = emailTot.includes(newEmail)
+
+if (isPresent == true) {
+  console.log("L'utente è presente")
+}
+
+else {
+  console.log ("L'utente non è registrato")
+
+}
